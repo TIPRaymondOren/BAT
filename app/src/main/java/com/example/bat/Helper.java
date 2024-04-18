9d8b4aaf159a05cac5e8189d2a48b029b37871c7
TIPRaymondOren <mrjoren@tip.edu.ph>
2024-04-18 22:15:48 +0800

    Added Helper Java Class
    Kindly read the comments in the Helper.java file for more information

---
+++ app/src/main/java/com/example/bat/Helper.java
@@ -0,0 +1,6 @@
+package com.example.bat;
+
+public class Helper {
+//    TODO: Getters and Setters here.
+//    NOTE FOR WARREN: Check your activity in loginsignup on how to do the Helper java class.
+}
