7083b4f3908458fd541e1fd6f693339f2ab42665
nicholesales <snichole.0714@gmail.com>
2024-04-21 14:23:23 +0800

    Added Push Notification
    - inserted notification code in loading screen class papuntang mechanic deets
    - inserted perms for notif in manifest
    - added notif icon in drawable

--- app/src/main/java/com/example/bat/LoadingScreen.java
+++ app/src/main/java/com/example/bat/LoadingScreen.java
@@ -1,12 +1,23 @@
 package com.example.bat;
 
+import android.Manifest;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -30,6 +41,17 @@ public class LoadingScreen extends AppCompatActivity {
         int loadProcess3Delay = 8000;
 
         backEndText = findViewById(R.id.backEndProcess);
+
+        //check if app has perms for notifs
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
+            if (ContextCompat.checkSelfPermission(LoadingScreen.this,
+                    android.Manifest.permission.POST_NOTIFICATIONS) !=
+                    PackageManager.PERMISSION_GRANTED){
+
+                ActivityCompat.requestPermissions(LoadingScreen.this,
+                        new String[]{Manifest.permission.POST_NOTIFICATIONS}, 101);
+            }
+        }
 
         // --------------------- Loading Process Description #2 ---------------------
         loadProcessHandler1.postDelayed(new Runnable() {
@@ -60,9 +82,36 @@ public class LoadingScreen extends AppCompatActivity {
             public void run() {
                 Intent openScreen = new Intent(LoadingScreen.this, MechanicPage.class);
                 LoadingScreen.this.startActivity(openScreen);
+                makeNotification();
             }
         }, loadingTimeInMS);
 
+
+    }
+
+    public void makeNotification(){
+        String chanelID = "CHANNEL_ID_NOTIFICATION";
+        String info = "Hang tight! The mechanic will be with you shortly.";
+
+        //create notification
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext(), chanelID);
+
+        builder.setSmallIcon(R.drawable.baseline_notifications_active_24)
+                .setContentTitle("Mechanic Found")
+                .setContentText(info)
+                .setAutoCancel(true)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        //launch mechanic page when notification is clicked
+        Intent intent = new Intent(getApplicationContext(), MechanicPage.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(),
+                0, intent, PendingIntent.FLAG_MUTABLE);
+
+        //display notification
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(0, builder.build());
 
     }
 }
