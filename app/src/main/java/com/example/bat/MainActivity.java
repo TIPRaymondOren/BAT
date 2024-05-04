d86b0b7c56d14b94073021866df58390194fa6f5
annedeguzman <mjdeguzman1@tip.edu.ph>
2024-05-05 03:17:17 +0800

    Updated UIs
    > Main Activity Screen

--- app/src/main/java/com/example/bat/MainActivity.java
+++ app/src/main/java/com/example/bat/MainActivity.java
@@ -1,209 +1,26 @@
 package com.example.bat;
 
-// TODO: Remove the TODOs once the layout for report screen is done
-// TODO: Remove this code after geolocation testing
-import android.app.AlertDialog;
-import android.annotation.TargetApi;
-import android.content.DialogInterface;
-import android.content.pm.PackageManager;
-import android.os.Build;
-import android.util.Log;
-import android.widget.Toast;
-// TODO: End of code for geolocation testing
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-// TODO: Remove this code after geolocation testing
-import java.util.ArrayList;
-
-import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
-import static android.Manifest.permission.ACCESS_FINE_LOCATION;
-
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-// TODO: End of code for geolocation testing
-
 public class MainActivity extends AppCompatActivity {
 
-    // TODO: Remove this code after geolocation testing
-    private ArrayList<String> permissionsToRequest;
-    private ArrayList<String> permissionsRejected = new ArrayList<>();
-    private ArrayList<String> permissions = new ArrayList<>();
-    private final static int ALL_PERMISSIONS_RESULT = 101;
-    GeoLocation locationTrack;
-    // TODO: End of code for geolocation testing
-
-    // TODO: Remove this after firebase testing
-    private DatabaseReference reference;
-//    TODO: End of firebase testing
-    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // TODO: Remove this code after geolocation testing
-        permissions.add(ACCESS_FINE_LOCATION);
-        permissions.add(ACCESS_COARSE_LOCATION);
-        permissionsToRequest = findUnAskedPermissions(permissions);
-        // TODO: End of code for geolocation testing
-
-        btn  = findViewById(R.id.button);
-
-        btn.setOnClickListener(new View.OnClickListener() {
+        Button startButton = findViewById(R.id.startbtn);
+        startButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-
-                Intent intent = new Intent(getApplicationContext(), MechanicPage.class);
+            public void onClick(View v) {
+                // Access the login screen
+                Intent intent = new Intent(MainActivity.this, Login.class);
                 startActivity(intent);
             }
-         });
-
-        Button geoButton = findViewById(R.id.geobtn);
-
-        geoButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                locationTrack = new GeoLocation(MainActivity.this);
-
-
-                if (locationTrack.canGetLocation()) {
-
-
-                    double longitude = locationTrack.getLongitude();
-                    double latitude = locationTrack.getLatitude();
-
-                    Toast.makeText(getApplicationContext(), "Longitude:" + Double.toString(longitude) + "\nLatitude:" + Double.toString(latitude), Toast.LENGTH_SHORT).show();
-                    Log.i("Location Data", "Longitude: " + Double.toString(longitude) + "   |||   " + "Latitude: " + Double.toString(latitude));
-                } else {
-                    locationTrack.showSettingsAlert();
-                }
-
-            }
         });
-
-//        TODO: Remove this when implementing the firebase, this is to be used as reference only
-
-        Button firebaseBtn = findViewById(R.id.frbsBtn);
-        reference = FirebaseDatabase.getInstance().getReference("message");
-    firebaseBtn.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            reference.setValue("Successful Entry");
-        };
-    });
-    // TODO: End of firebase reference code
-
-        // TODO: Code for testing Loading Page (remove after testing and implementation)
-
-        Button loadingPageBtn = findViewById(R.id.loadBtn);
-        loadingPageBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent loadIntent = new Intent(MainActivity.this, LoadingScreen.class);
-                startActivity(loadIntent);
-            }
-        });
-        // TODO: End of Code for testing Loading Page (remove after testing and implementation)
-
-        // TODO: Code for testing EndScreen
-
-        Button endScrnBtn = findViewById(R.id.endScrnBtn);
-        endScrnBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent endIntent = new Intent(MainActivity.this, EndScreen.class);
-                startActivity(endIntent);
-            }
-        });
-
-        // TODO: End of Code for testing EndScreen
     }
-    // TODO: Remove this code after geolocation testing
-    private ArrayList<String> findUnAskedPermissions(ArrayList<String> wanted) {
-        ArrayList<String> result = new ArrayList<String>();
-
-        for (String perm : wanted) {
-            if (!hasPermission(perm)) {
-                result.add(perm);
-            }
-        }
-
-        return result;
-    }
-
-    private boolean hasPermission(String permission) {
-        if (canMakeSmores()) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                return (checkSelfPermission(permission) == PackageManager.PERMISSION_GRANTED);
-            }
-        }
-        return true;
-    }
-
-    private boolean canMakeSmores() {
-        return (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP_MR1);
-    }
-
-
-    @TargetApi(Build.VERSION_CODES.M)
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        switch (requestCode) {
-
-            case ALL_PERMISSIONS_RESULT:
-                for (String perms : permissionsToRequest) {
-                    if (!hasPermission(perms)) {
-                        permissionsRejected.add(perms);
-                    }
-                }
-
-                if (permissionsRejected.size() > 0) {
-
-
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                        if (shouldShowRequestPermissionRationale(permissionsRejected.get(0))) {
-                            showMessageOKCancel("These permissions are mandatory for the application. Please allow access.",
-                                    new DialogInterface.OnClickListener() {
-                                        @Override
-                                        public void onClick(DialogInterface dialog, int which) {
-                                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                                                requestPermissions(permissionsRejected.toArray(new String[permissionsRejected.size()]), ALL_PERMISSIONS_RESULT);
-                                            }
-                                        }
-                                    });
-                            return;
-                        }
-                    }
-
-                }
-
-                break;
-        }
-
-    }
-
-    private void showMessageOKCancel(String message, DialogInterface.OnClickListener okListener) {
-        new AlertDialog.Builder(MainActivity.this)
-                .setMessage(message)
-                .setPositiveButton("OK", okListener)
-                .setNegativeButton("Cancel", null)
-                .create()
-                .show();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        locationTrack.stopListener();
-    }
-    // TODO: End of code for geolocation testing
 }
